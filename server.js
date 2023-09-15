7d50ce6421a7747acb70a293207756e4905e6292
hmanne23 <143413901+hmanne23@users.noreply.github.com>
2023-09-14 22:49:33 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,34 +1,50 @@
 
 const express = require('express');
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
 
-const budget = { 
-  myBudget: [
-  {
-    title: 'Extras',
-    budget: 50
-  },
-  {
-    title: 'Grocery-List',
-    budget :150
-  },
-  {
-    title: 'Rent',
-    budget:525
-  }
-]
-};
+// const budget = { 
+//   myBudget: [
+//   {
+//     title: 'Extras',
+//     budget: 50
+//   },
+//   {
+//     title: 'Grocery-List',
+//     budget :150
+//   },
+//   {
+//     title: 'Rent',
+//     budget:525
+//   }
+// ]
+// };
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-  res.json(budget);
+
+  fs.readFile("pb.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading JSON file:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    try {
+      const pbData = JSON.parse(data);
+      res.json(pbData);
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
 });
 
 app.listen(port, () => {
