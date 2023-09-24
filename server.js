1ebaa0e29986f750b48014a3961ada6a0c7ba5d9
hmanne23 <143413901+hmanne23@users.noreply.github.com>
2023-09-24 18:46:33 -0400

    cors

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
+//Budget API
 
 const express = require('express');
-const fs = require("fs");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
-
+// app.use('/', express.static('public'));
+app.use(cors());
 
 // const budget = { 
 //   myBudget: [
@@ -24,29 +25,31 @@ app.use('/', express.static('public'));
 // ]
 // };
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/hello', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 app.get('/budget', (req, res) => {
+  const samp=require('./pb.json')
+  res.json(samp);
 
-  fs.readFile("pb.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading JSON file:", err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
+  // fs.readFile("pb.json", "utf8", (err, data) => {
+  //   if (err) {
+  //     console.error("Error reading JSON file:", err);
+  //     res.status(500).send("Internal Server Error");
+  //     return;
+  //   }
 
-    try {
-      const pbData = JSON.parse(data);
-      res.json(pbData);
-    } catch (error) {
-      console.error("Error parsing JSON:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  });
+  //   try {
+  //     const pbData = JSON.parse(data);
+  //     res.json(pbData);
+  //   } catch (error) {
+  //     console.error("Error parsing JSON:", error);
+  //     res.status(500).send("Internal Server Error");
+  //   }
+  // });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+    console.log(`Example API served at http://localhost:${port}!`);
 });
